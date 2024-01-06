6c18d66d928b72401aa4e924f4c97507ac25c20c
Juani <juanialvarezzm@gmail.com>
2024-01-06 04:45:40 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/home/Navbar';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import { AnimatedOnScroll } from "react-animated-css-onscroll";
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import NosotrosPage from './pages/NosotrosPage';
+import NuestraformaPage from './pages/NuestraFormaPage';
+import ServiciosPage from './pages/ServiciosPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className='navbar'>
+        <Navbar></Navbar>
+        </div>
+        <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/contact' element={<ContactPage/>} />
+            <Route path='/nosotros' element={<NosotrosPage/>} />
+            <Route path='/trabajos' element={<NuestraformaPage/>} />
+            <Route path='/servicios' element={<ServiciosPage/>} />
+        </Routes>
+      </div>
     </div>
   );
 }
