b158103fa2eba6a496afff293938d60ca2e5c338
Juani <juanialvarezzm@gmail.com>
2024-01-06 04:44:04 +0100

    data

---
+++ src/data/data.js
@@ -0,0 +1,16 @@
+    export const slides =[
+      {
+        src: "https://www.atebits.com/wp-content/uploads/2019/12/word-image-19.jpeg",
+        alt: "Image 1 for carousel"
+      },
+      {
+        src: "https://gruastransportescn.com/wp-content/uploads/2022/07/trayectoria-portada-1.jpg",
+        alt: "Image 2 for carousel"
+      },
+      {
+        src: "https://http2.mlstatic.com/D_NQ_NP_619660-MLA70421235709_072023-O.webp",
+        alt: "Image 3 for carousel"
+      }
+]
+// https://picsum.photos/seed/img3/600/400
+
